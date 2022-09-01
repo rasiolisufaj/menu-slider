28c661745f7dffa9a293b3055c8390e27f9a25c2
Rasiol Isufaj <rasiolisufaj47@gmail.com>
2022-09-01 22:26:57 +0200

    signup effect

--- script.js
+++ script.js
@@ -3,4 +3,18 @@ const modal = document.getElementById("modal");
 const open = document.getElementById("open");
 const close = document.getElementById("close");
 
-toggle.addEventListener("click", () => document.body.classList.toggle("hello"));
+toggle.addEventListener("click", () =>
+  document.body.classList.toggle("show-nav")
+);
+
+// Show the SignUp form
+open.addEventListener("click", () => modal.classList.add("show-modal"));
+
+// Hide the SignUp form
+close.addEventListener("click", () => modal.classList.remove("show-modal"));
+
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.classList.remove("show-modal");
+  }
+});
