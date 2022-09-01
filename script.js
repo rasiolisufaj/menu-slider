ae8a4c0fc1750a7d905a6c4433dd187858f2bc21
Rasiol Isufaj <rasiolisufaj47@gmail.com>
2022-09-01 19:06:29 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggle = document.getElementById("toggle");
+const modal = document.getElementById("modal");
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+
+toggle.addEventListener("click", () => document.body.classList.toggle("hello"));
